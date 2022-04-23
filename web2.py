0bf95c2f6c6d6476103eb85d965014ba76aad63f
Sam Berman <samjberman0@gmail.com>
2022-04-23 14:23:53 -0400

    beginnings of web app, Tufts roster, and images directory

--- web2.py
+++ web2.py
@@ -1,5 +1,32 @@
 import streamlit as st
+import csv
+import pandas as pd
+from PIL import Image
 
 st.write("""
-         # This is our sample website using python!
+         ## Monte Carlo Simulations for Batting Order Optimization (Change Me Later)
          """)
+
+team = st.selectbox('Select a team:', ['No Selection', 'Amherst', 'Bates', 
+                    'Bowdoin', 'Colby', 'Conn. College', 'Hamilton',
+                    'Middlebury', 'Trinity', 'Tufts', 'Wesleyan', 'Williams'])
+
+if team != 'No Selection':
+    st.write('You selected:', team)
+
+    # get the team name and read in the data from the csv for that team
+    filename = team + "_roster.csv"
+    st.write(filename)
+    team_data = pd.read_csv(filename)
+    players = team_data.Name
+
+if team == 'Tufts':
+    logo = Image.open('images/tufts-logo.png')
+    st.image(logo, caption='Tufts Logo')
+
+    options = st.multiselect('Select the players in the lineup', players)
+
+    st.write(options)
+    #st.write(options[0])
+
+
