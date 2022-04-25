be6c22471b2eb995d558ec10bdf07625692f9665
Sam Berman <samjberman0@gmail.com>
2022-04-25 13:19:47 -0400

    nescac logo and some layour updates

--- roster-optimization.py
+++ roster-optimization.py
@@ -1,4 +1,20 @@
-from asyncio import run
+# 
+# CS 152: Sports Analytics
+# Spring 2022
+# Final Project
+# NESCAC Baseball Batting Order Optimization
+#
+# Authors: Samuel Berman       (sberma04)
+#          Alex Maleno         (amalen01)
+#          Thomas "Clay" Kynor (tkynor01)
+#
+# Purpose: A Streamlit web application that allows users
+#          to select a NESCAC team and a list of players from
+#          that team. Program them runs a Monte Carlo simulation
+#          of those players to determine the average number of
+#          runs that batting lineup would score.
+#
+
 import streamlit as st
 import csv
 import pandas as pd
@@ -7,6 +23,15 @@ from PIL import Image
 import random
 from itertools import permutations
 import time
+
+st.set_page_config(
+     page_title="NESCAC Baseball Batting Order Optimization",
+     page_icon="images/nescac-logo.png"
+)
+
+#
+# Utility Functions
+#
 
 def read_team_data(filename):
     team_data = pd.read_csv(filename)
@@ -252,101 +277,81 @@ def optimize_lineup(df, players):
     end_time = time.perf_counter()
     st.write(end_time - start_time)
 
-    
+def main():
+    st.title("NESCAC Baseball Batting Order Optimization")
 
-st.write("""
-         ## Monte Carlo Simulations for Batting Order Optimization (Change Me Later)
-         """)
+    team_list = ['No Selection', 'Amherst', 'Bates', 
+                'Bowdoin', 'Colby', 'Hamilton', 'Middlebury', 
+                'Trinity', 'Tufts', 'Wesleyan', 'Williams']
 
-team_list = ['No Selection', 'Amherst', 'Bates', 
-             'Bowdoin', 'Colby', 'Hamilton', 'Middlebury', 
-             'Trinity', 'Tufts', 'Wesleyan', 'Williams']
+    # col1, col2 = st.columns(2)
+    # with col1:
+    your_team = st.selectbox('Select Your Team:', team_list)
 
-# col1, col2 = st.columns(2)
-# with col1:
-your_team = st.selectbox('Select Your Team:', team_list)
+    if your_team != 'No Selection':
+        st.write('You selected:', your_team)
 
-if your_team != 'No Selection':
-    st.write('You selected:', your_team)
+        uploaded_file = st.file_uploader("Optional: Upload a new csv data file", type='csv')
+        if uploaded_file is not None:
+            # To read file as bytes:
+            bytes_data = uploaded_file.getvalue()
+            st.write(bytes_data)
 
-    uploaded_file = st.file_uploader("Optional: Upload a new csv data file", type='csv')
-    if uploaded_file is not None:
-        # To read file as bytes:
-        bytes_data = uploaded_file.getvalue()
-        st.write(bytes_data)
+            dataframe = pd.read_csv(uploaded_file)
+            st.write(dataframe)
 
-        # # To convert to a string based IO:
-        # stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        # st.write(stringio)
+        logo_filename = your_team + '-logo.png'
+        logo = Image.open('images/' + logo_filename)
+        cap = your_team + ' Logo'
 
-        # # To read file as string:
-        # string_data = stringio.read()
-        # st.write(string_data)
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.write('')
+        with col2:
+            st.image(logo)
+        with col3:
+            st.write('')
 
-        # Can be used wherever a "file-like" object is accepted:
-        dataframe = pd.read_csv(uploaded_file)
-        st.write(dataframe)
+    # with col2:
+    #     opponent = st.selectbox('Select Your Opponent:', team_list)
 
-    logo_filename = your_team + '-logo.png'
-    logo = Image.open('images/' + logo_filename)
-    cap = your_team + ' Logo'
+    #     if opponent != 'No Selection':
+    #         st.write('You selected:', opponent)
+    #         logo_filename2 = opponent + '-logo.png'
+    #         logo2 = Image.open('images/' + logo_filename2)
+    #         cap2 = opponent + ' Logo'
+    #         st.image(logo2)
+            
 
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.write('')
-    with col2:
-        st.image(logo)
-    with col3:
-        st.write('')
+    if your_team != 'No Selection':
 
-# with col2:
-#     opponent = st.selectbox('Select Your Opponent:', team_list)
+        # get the your_team name and read in the data from the csv for that your_team
+        filename = 'rosters/' + your_team + '_roster.csv'
 
-#     if opponent != 'No Selection':
-#         st.write('You selected:', opponent)
-#         logo_filename2 = opponent + '-logo.png'
-#         logo2 = Image.open('images/' + logo_filename2)
-#         cap2 = opponent + ' Logo'
-#         st.image(logo2)
         
+        your_team_data = pd.read_csv(filename)
+        your_team_players = your_team_data.Player
 
-if your_team != 'No Selection':
+        run_type = st.radio("Select an option:", ['# Of Runs', 'Lineup Optimization'])
 
-    # get the your_team name and read in the data from the csv for that your_team
-    filename = 'rosters/' + your_team + '_roster.csv'
+        message1 = 'Select the players for the ' + your_team + ' lineup:'
+        your_team_selected = st.multiselect(message1, your_team_players)
 
-    
-    your_team_data = pd.read_csv(filename)
-    your_team_players = your_team_data.Player
+        #st.write(your_team_selected)
 
-    run_type = st.radio("Select an option:", ['# Of Runs', 'Lineup Optimization'])
-
-    message1 = 'Select the players for the ' + your_team + ' lineup:'
-    your_team_selected = st.multiselect(message1, your_team_players)
-
-    #st.write(your_team_selected)
-
-    if st.button('Submit'):
-        df = read_team_data(filename)
-    
-        if run_type == '# Of Runs':
-            thresholds = calculate_thresholds(df, your_team_selected)
-            st.title("Number of predicted runs: " + str(avg_runs(thresholds)))
-        else:
-            st.title('Fuck Alex')
-            optimize_lineup(df, your_team_selected)
+        if st.button('Submit'):
+            df = read_team_data(filename)
+        
+            if run_type == '# Of Runs':
+                thresholds = calculate_thresholds(df, your_team_selected)
+                st.title("Number of predicted runs: " + str(avg_runs(thresholds)))
+            else:
+                st.title('Fuck Alex')
+                optimize_lineup(df, your_team_selected)
 
 
-# if opponent != 'No Selection':
+if __name__ == "__main__":
+    main()
 
-#     # get the your_team name and read in the data from the csv for that your_team
-#     filename2 = opponent + '_roster.csv'
 
-#     opponent_data = pd.read_csv('rosters/' + filename2)
-#     opponent_players = opponent_data.Player
-
-#     message2 = 'OPTIONAL: Sel' + opponent + ' lineup:'
-#     opponent_selected = st.multiselect(message2, opponent_players)
-
-#     st.write(opponent_selected)
 
