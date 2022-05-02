dcff0a4c2401e164dee57e585e93376963c81546
Sam Berman <samjberman0@gmail.com>
2022-05-01 23:00:57 -0400

    compare two lineups; upload new csv; refactored main

--- roster-optimization.py
+++ roster-optimization.py
@@ -15,8 +15,10 @@
 #          runs that batting lineup would score.
 #
 
-from audioop import avg
-from pydoc import tempfilepager
+# from audioop import avg
+# from pydoc import tempfilepager
+# from sys import orig_argv
+from webbrowser import get
 import streamlit as st
 import csv
 import pandas as pd
@@ -26,18 +28,15 @@ import random
 from itertools import permutations
 import time
 
-st.set_page_config(
-     page_title="NESCAC Baseball Batting Order Optimization",
-     page_icon="images/nescac-logo.png"
-)
+
 
 #
 # Utility Functions
 #
 
-def read_team_data(filename):
-    team_data = pd.read_csv(filename)
-    df = pd.DataFrame(team_data)
+def read_team_data(data):
+    # team_data = pd.read_csv(filename)
+    df = pd.DataFrame(data)
 
     return df
 
@@ -69,9 +68,6 @@ def calculate_thresholds(df, players):
         curr_threshold = 0
         if df['Player'][i] not in players:
             continue
-        
-        # thresholds[df['Player'][i]] = []
-
         
         at_bats = df['AB'][i]
         outs = (df['AB'][i] - df['H'][i] - df['BB'][i] - df['HBP'][i] - df['SO'][i])
@@ -123,12 +119,11 @@ def calculate_thresholds(df, players):
         else:
             thresholds['hr_thresh'].append(curr_threshold)
 
-    # st.write(thresholds)
     return thresholds
 
 
 def run_monte_carlo(thresholds):
-    #start = time.perf_counter()
+
     game_state = dict({'1B': 0, '2B': 0, '3B':0, 'Outs': 0, 'Runs':0, 'Next_Batter':0, 'Inning':1})
 
     for i in range(1,10):
@@ -235,49 +230,14 @@ def run_monte_carlo(thresholds):
 
             game_state['Next_Batter'] = (game_state['Next_Batter'] + 1) % (len(thresholds['1b_thresh']))
 
-    #end = time.perf_counter()
-    #st.write(end - start)
     return game_state['Runs']
 
 def avg_runs(thresholds):
-    # start_time = time.perf_counter()
     runs = []
     for i in range(1000):
         runs.append(run_monte_carlo(thresholds))
 
-    # end_time = time.perf_counter()
-    # st.write(end_time - start_time)
     return (sum(runs) / len(runs))
-
-def optimize_lineup(df, players):
-    # st.write("Permutations: ")
-    # st.write(list(permutations(players)))
-
-    perms = list(permutations(players))
-    curr_max = 0
-    curr_best_lineup = []
-    #start_time = time.perf_counter()
-
-    for perm in perms:
-        thresholds = calculate_thresholds(df, perm)
-        avg = run_monte_carlo(thresholds)
-
-        # for i in range(1000):
-        #     runs.append(run_monte_carlo(thresholds))
-        # avg = (sum(runs) / len(runs))
-
-        # st.write(avg)
-        # st.write(perm)
-        # st.write('-------')
-        if avg > curr_max:
-            curr_max = avg
-            curr_best_lineup = perm
-
-    #st.write("Best: ")
-    #st.write(curr_max)
-    #st.write(curr_best_lineup)
-    #end_time = time.perf_counter()
-    #st.write(end_time - start_time)
 
 def flip(players, i, j):
     players[i], players[j] = players[j], players[i]
@@ -288,39 +248,195 @@ def do_flip_optimization(df, players):
     thresholds = calculate_thresholds(df, curr_players)
     best_runs = avg_runs(thresholds)
     best_lineup = curr_players
-    # st.write(curr_players)
-    # st.write(best_runs)
-    # st.write ('----------')
     
     for i in range(len(players) - 2): # - 1 so we can flip the end w/o OOB
         curr_players = flip(players[:], i, i + 1)
         thresholds = calculate_thresholds(df, curr_players)
         runs = avg_runs(thresholds)
-        # st.write(curr_players)
-        # st.write(runs)
-        # st.write ('----------')
+
         if runs > best_runs:
             best_runs = runs
             best_lineup = curr_players
 
     return best_lineup, best_runs
 
-    # recurse and do_flip_optimization w the best lineup we found
-    # if best_lineup != players:
-    #     st.write(best_lineup)
-    #     return do_flip_optimization(df, best_lineup)
-    # else:
-    #     return best_lineup
+def check_file(data):
+    # make sure the appropriate columns exist
+    cols = ['Player', 'AB', 'SO', 'HBP', 'BB', '2B', '3B', 'HR']
+    for col in cols:
+        if col not in data.columns:
+            return False
+
+    # make sure there are at least 9 players
+    if len(data.index) < 9:
+        return False
+
+    return True
+
+def get_new_data():
+    uploaded_file = st.file_uploader("Upload a new csv data file", type='csv')
+    if uploaded_file is not None:
+        dataframe = pd.read_csv(uploaded_file)
+        return dataframe
+
+
+def run_compare_lineups(your_team_players, data):
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write('#### Select the players for lineup 1:')
+        one   = st.selectbox('Lead-Off: ', your_team_players)
+        two   = st.selectbox('Second: ', your_team_players)
+        three = st.selectbox('Third: ', your_team_players)
+        four  = st.selectbox('Clean Up: ', your_team_players)
+        five  = st.selectbox('Fifth: ', your_team_players)
+        six   = st.selectbox('Sixth: ', your_team_players)
+        seven = st.selectbox('Seventh: ', your_team_players)
+        eight = st.selectbox('Eighth: ', your_team_players)
+        nine  = st.selectbox('Ninth: ', your_team_players)
+    with col2:
+        st.write('#### Select the players for lineup 2:')
+        one2   = st.selectbox('Lead-Off:  ', your_team_players)
+        two2   = st.selectbox('Second:  ', your_team_players)
+        three2 = st.selectbox('Third:  ', your_team_players)
+        four2  = st.selectbox('Clean Up:  ', your_team_players)
+        five2  = st.selectbox('Fifth:  ', your_team_players)
+        six2   = st.selectbox('Sixth:  ', your_team_players)
+        seven2 = st.selectbox('Seventh:  ', your_team_players)
+        eight2 = st.selectbox('Eighth:  ', your_team_players)
+        nine2  = st.selectbox('Ninth:  ', your_team_players)
+
+    if st.button('Submit'):
+        lineup1 = [one, two, three, four, five, six, seven, eight, nine]
+        lineup2 = [one2, two2, three2, four2, five2, six2, seven2, eight2, nine2]
+        
+        players_set1 = set(lineup1)
+        players_set2 = set(lineup2)
+
+        if '<select>' in players_set1 or '<select>' in players_set2:
+            st.error('Select a player for each position in the lineup')
+        elif len(players_set1)!= 9 or len(players_set2) != 9:
+            st.error('No duplicates allowed')
+        elif lineup1 == lineup2:
+            st.error('Lineups cannot be the same')
+        else:
+            df = read_team_data(data)
+
+            thresh1 = calculate_thresholds(df, lineup1)
+            thresh2 = calculate_thresholds(df, lineup2)
+            avg1 = avg_runs(thresh1)
+            avg2 = avg_runs(thresh2)
+
+            st.write("""## Better Lineup:""")
+            if avg1 > avg2:
+                st.write("Lineup #1")
+                st.write(lineup1)
+            else:
+                st.write("Lineup #2")
+                st.write(lineup2)
+
+def run_predicted_runs(your_team_players, data, team_name):
+    st.write(' ### Select the players for the ' + team_name + ' lineup:')
+
+    col1, col2 = st.columns(2)
+    with col1:
+        one = st.selectbox('Lead-Off: ', your_team_players)
+        three = st.selectbox('Third: ', your_team_players)
+        five = st.selectbox('Fifth: ', your_team_players)
+        seven = st.selectbox('Seventh: ', your_team_players)
+        nine = st.selectbox('Ninth: ', your_team_players)
+    with col2:
+        two = st.selectbox('Second: ', your_team_players)
+        four = st.selectbox('Clean Up: ', your_team_players)
+        six = st.selectbox('Sixth: ', your_team_players)
+        eight = st.selectbox('Eighth: ', your_team_players)
+
+    if st.button('Submit'):
+        lineup = [one, two, three, four, five, six, seven, eight, nine]
+        lineup_set = set(lineup)
+
+        if '<select>' in lineup_set:
+            st.error('Select a player for each position in the lineup')
+        elif len(lineup_set) != 9:
+            st.error('No duplicates allowed')
+        else:
+            df = read_team_data(data)
+        
+            thresholds = calculate_thresholds(df, lineup)
+            st.title("Number of predicted runs: " + str(avg_runs(thresholds)))
+            
+
+def run_lineup_optimization(your_team_players, data, team_name):
+    st.write(' ### Select the players for the ' + team_name + ' lineup:')
+
+    col1, col2 = st.columns(2)
+    with col1:
+        one = st.selectbox('Lead-Off: ', your_team_players)
+        three = st.selectbox('Third: ', your_team_players)
+        five = st.selectbox('Fifth: ', your_team_players)
+        seven = st.selectbox('Seventh: ', your_team_players)
+        nine = st.selectbox('Ninth: ', your_team_players)
+    with col2:
+        two = st.selectbox('Second: ', your_team_players)
+        four = st.selectbox('Clean Up: ', your_team_players)
+        six = st.selectbox('Sixth: ', your_team_players)
+        eight = st.selectbox('Eighth: ', your_team_players)
+
+    if st.button('Submit'):
+        lineup = [one, two, three, four, five, six, seven, eight, nine]
+        lineup_set = set(lineup)
+
+        if '<select>' in lineup_set:
+            st.error('Select a player for each position in the lineup')
+        elif len(lineup_set) != 9:
+            st.error('No duplicates allowed')
+        else:
+            df = read_team_data(data)
+        
+            max = 15
+            iterations = 1
+            original = lineup[:]
+
+            its = st.empty()
+            loading_bar = st.progress(0)
+
+            while iterations <= max:
+                prog = iterations / 15
+        
+                its.text(f'Loading {prog*100:.2f}%')
+                loading_bar.progress(prog)
+            
+                lineup, runs = do_flip_optimization(df, original)
+                if lineup == original:
+                    prog = 1.0
+                    loading_bar.progress(prog)
+                    its.text('100%')
+                    break
+                else: 
+                    original = lineup[:]
+
+                iterations += 1
+
+            col1, col2 = st.columns(2)
+            with col1:
+                st.write("""## Original Lineup:""")
+                st.write(lineup)
+            with col2:
+                st.write("""## Optimized Lineup:""")
+                st.write(lineup)
+
 
 def main():
+    st.set_page_config(
+        page_title="NESCAC Baseball Batting Order Optimization",
+        page_icon="images/nescac-logo.png"
+    )
+
     st.title("NESCAC Baseball Batting Order Optimization")
 
     team_list = ['<select>', 'Amherst', 'Bates', 
                 'Bowdoin', 'Colby', 'Hamilton', 'Middlebury', 
                 'Trinity', 'Tufts', 'Wesleyan', 'Williams']
 
-    # col1, col2 = st.columns(2)
-    # with col1:
     your_team = st.selectbox('Select Your Team:', team_list)
 
     if your_team != '<select>':
@@ -328,7 +444,6 @@ def main():
 
         logo_filename = your_team + '-logo.png'
         logo = Image.open('images/' + logo_filename)
-        cap = your_team + ' Logo'
 
         col1, col2, col3 = st.columns(3)
         with col1:
@@ -338,83 +453,42 @@ def main():
         with col3:
             st.write('')
 
-        uploaded_file = st.file_uploader("Optional: Upload a new csv data file", type='csv')
-        if uploaded_file is not None:
-            # To read file as bytes:
-            bytes_data = uploaded_file.getvalue()
-            #st.write(bytes_data)
 
-            dataframe = pd.read_csv(uploaded_file)
-            st.write(dataframe)
+        data_location = st.radio('Would you like to use 2021 Season data or upload a new file?', 
+                                 ['2021 Season', 'Upload'])
 
-    # with col2:
-    #     opponent = st.selectbox('Select Your Opponent:', team_list)
-
-    #     if opponent != 'No Selection':
-    #         st.write('You selected:', opponent)
-    #         logo_filename2 = opponent + '-logo.png'
-    #         logo2 = Image.open('images/' + logo_filename2)
-    #         cap2 = opponent + ' Logo'
-    #         st.image(logo2)
-    
-
-    if your_team != '<select>':
-
-        # get the your_team name and read in the data from the csv for that your_team
-        filename = 'rosters/' + your_team + '_roster.csv'
-
-        
-        your_team_data = pd.read_csv(filename)
-        your_team_players = your_team_data.Player
-        your_team_players = list(your_team_players)
-        your_team_players.insert(0, '<select>')
-
-        run_type = st.radio("Select an option:", ['Predicted # Of Runs', 
-                                                  'Lineup Optimization',
-                                                  'Compare Two Lineups'])
-
-
-        if run_type == 'Compare Two Lineups':
-            st.warning('Comparing lineups is coming soon!')
+        valid = False
+        if data_location == 'Upload':
+            your_team_data = get_new_data()
+            if your_team_data is not None:
+                valid = check_file(your_team_data)
+                if not valid:
+                    st.error("try again fool")
         else:
-            message1 = 'Select the players for the ' + your_team + ' lineup:'
-            #your_team_selected = st.multiselect(message1, your_team_players)
+            filename = 'rosters/' + your_team + '_roster.csv'
+            your_team_data = pd.read_csv(filename)
+            valid = True
 
-            st.write(message1)
-            col1, col2 = st.columns(2)
-            with col1:
-                one = st.selectbox('Lead-Off: ', your_team_players)
-                three = st.selectbox('Third: ', your_team_players)
-                five = st.selectbox('Fifth: ', your_team_players)
-                seven = st.selectbox('Seventh: ', your_team_players)
-                nine = st.selectbox('Clean Up: ', your_team_players)
-            with col2:
-                two = st.selectbox('Second: ', your_team_players)
-                four = st.selectbox('Fourth: ', your_team_players)
-                six = st.selectbox('Sixth: ', your_team_players)
-                eight = st.selectbox('Eighth: ', your_team_players)
+        if valid:
+            your_team_players = your_team_data.Player
+            your_team_players = list(your_team_players)
+            your_team_players.insert(0, '<select>')
 
-            if st.button('Submit'):
-                players_test = [one, two, three, four, five, six, seven, eight, nine]
-                players_test
-                
-                players_set = set(players_test)
-                if len(players_set) != 9:
-                    st.error('No duplicates allowed')
-                elif '<select>' in players_set:
-                    st.error('Select a player for each position in the lineup')
-                else:
-                    df = read_team_data(filename)
-                
-                    if run_type == 'Predicted # Of Runs':
-                        thresholds = calculate_thresholds(df, players_test)
-                        st.title("Number of predicted runs: " + str(avg_runs(thresholds)))
-                    else: # run_type == 'Lineup Optimization':
-                        # st.title('Fuck Alex')
-                        lineup, runs = do_flip_optimization(df, players_test)
-                        st.title('Optimized Lineup:')
-                        st.write(lineup)
-                        #st.write(runs)
+            run_type = st.radio("Select an option:", 
+                                ['Predicted # Of Runs', 
+                                    'Lineup Optimization',
+                                    'Compare Two Lineups'])
+
+            if run_type == "Compare Two Lineups":
+                run_compare_lineups(your_team_players, your_team_data)
+                # st.write('compare')
+            elif run_type == "Predicted # Of Runs":
+                run_predicted_runs(your_team_players, your_team_data, your_team)
+                st.write('predict')
+            else:
+                run_lineup_optimization(your_team_players, your_team_data, your_team)
+                st.write('optimize')
+                           
                     
 
 if __name__ == "__main__":
